706c49eb168d692891bdc9ccd831536e03d4524f
Andrés Ignacio Torres <andresitorresm@gmail.com>
2021-12-12 01:05:45 -0400

    chore: add missing docstrings

--- tests/test_gutenberg2kindle.py
+++ tests/test_gutenberg2kindle.py
@@ -1,5 +1,9 @@
+"""General unit tests for the project"""
+
 from gutenberg2kindle import __version__
 
 
 def test_version():
+    """Ensure that Python's version variable has the expected value"""
+
     assert __version__ == '0.1.0'
